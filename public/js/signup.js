df5bcc82bfa6070dad6525ecebe13e513db56752
Soumya Kalluri <soumya.kalluri11@gmail.com>
2021-02-26 01:11:48 -0800

    fixed dead end signup button

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+'use strict';
+var database = [];
+$.getJSON('../database/database.json', function(data) {
+    database = data.users;
+    var dataLength = data.length;
+    for ( var i = 0; i < dataLength; i++ ) {
+        var obj = data.users[i];
+        database.push(obj);
+    }
+});
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+    console.log("login button initialized");
+    $("#signupButton").click(signup);
+}
+
+function signup(e) {
+    e.preventDefault();
+    document.location.href = "/";
+}
